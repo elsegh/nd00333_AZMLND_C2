5b8e4cf0c270fcc6ea5395d2fe53f86893992845
elsegh <150442224+elsegh@users.noreply.github.com>
2024-01-05 13:44:18 +0000

    Add files via upload
    file updated, now working correctly

--- endpoint.py
+++ endpoint.py
@@ -1,28 +1,32 @@
 import requests
 import json
+import os
+import ssl
 
 # URL for the web service, should be similar to:
 # 'http://8530a665-66f3-49c8-a953-b82a2d312917.eastus.azurecontainer.io/score'
 # scoring_uri = ''
 
-scoring_uri = 'http://cb68f2f6-066f-4f71-ac2f-cbe52241a827.westus2.azurecontainer.io/score'
+scoring_uri = 'http://4b064133-a240-49e9-9709-f6da8c69a306.southcentralus.azurecontainer.io/score'
 # If the service is authenticated, set the key or token
-key = '3oPtW4OnV9b8Kmb3PXBqpDCNi5P52qKw'
+key = 'GJeFy5E2rd2h9Tzc45taXCYdoI2scZhM'
 
-# Two sets of data to score, so we get two results back
-data = [
+# Two sets of data to score, so we get two results back "
+data={
+"Inputs":{   
+ "data": [
           {       
             "age": 17,
 	    "job": "blue-collar",
-	   "marital": "married",
+	    "marital": "married",
 	    "education": "university.degree",
             "default": "no",
 	    "housing": "yes",
             "loan": "yes",
 	    "contact": "cellular",
 	    "month": "may",
-	   "duration": 971,
-             "campaign": 1,
+	    "duration": 971,
+            "campaign": 1,
 	    "pdays": 999,
 	    "previous": 1,
 	    "poutcome": "failure",
@@ -30,9 +34,7 @@ data = [
             "cons.price.idx": 92.893,
 	    "cons.conf.idx": -46.2,
 	    "euribor3m": 1.299,
-	    "nr.employed": 5099.1
-            
-        
+	    "nr.employed": 5099.1          
           },
           {
             "age": 87,
@@ -44,7 +46,7 @@ data = [
 	    "loan": "yes",
 	    "contact": "cellular",
 	    "month": "may",
-	   "duration": 471,
+	    "duration": 471,
             "campaign": 1,
             "pdays": 999,
 	    "previous": 1,
@@ -54,10 +56,11 @@ data = [
        	    "cons.conf.idx": -46.2,
 	    "nr.employed": 5099.1,
             "euribor3m": 1.299
-           
-     
           }
-      ]
+	  ]
+ }
+ }
+# 
 # Convert to JSON string
 input_data = json.dumps(data)
 with open("data.json", "w") as _f:
